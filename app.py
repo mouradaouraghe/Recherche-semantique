9826bd749e5eb91a28619d76145c7d541e4aa6e5
Mourad Aouraghe <mourad.aouraghe@usmba.ac.ma>
2025-12-28 16:42:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ product_texts = [create_product_text(product) for product in products]
 
 # Create the embeddings from product_texts
 product_embeddings = create_embeddings(product_texts)
+def find_n_closest(query_vector, embeddings, n=3):
+  distances = []
+  for index, embedding in enumerate(embeddings):
+    # Calculate the cosine distance between the query vector and embedding
+    dist = distance.cosine(query_vector,embedding)
+    # Append the distance and index to distances
+    distances.append({"distance": dist, "index": index})
+  # Sort distances by the distance key
+  distances_sorted = sorted(distances,key=lambda x: x["distance"])
+  # Return the first n elements in distances_sorted
+  return distances_sorted[0:n]
